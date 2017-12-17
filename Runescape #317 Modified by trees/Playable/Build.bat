ccfda2ee62e1d70167415c2bd1e512c3e5376df9
emcry666 <34613829+emcry666@users.noreply.github.com>
2017-12-17 07:32:58 -0500

    Commit

--- Runescape #317 Modified by trees/Playable/Build.bat
+++ Runescape #317 Modified by trees/Playable/Build.bat
@@ -1,4 +1,4 @@
 @echo off
 title Building Files
-"C:\Program Files\Java\jdk1.7.0_07\bin\javac.exe" -d ./Class/ Java/*.java Java/sign/*.java
+"C:\Program Files (x86)\Java\jdk1.8.0_152\bin\javac.exe" -d ./Class/ Java/*.java Java/sign/*.java
 pause
