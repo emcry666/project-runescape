af779c1d3d9c5aadc9486b4fb6996ce9d70d9352
emcry666 <34613829+emcry666@users.noreply.github.com>
2017-12-17 21:16:10 -0500

    Changeling

--- rs3 files/Matrix RS3 Client/run local.bat
+++ rs3 files/Matrix RS3 Client/run local.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files (x86)\Java\jdk1.8.0_152\bin\java.exe" -Xms128m -Xmx800m -Xss2m -Dsun.java2d.noddraw=true -XX:CompileThreshold=10000 -cp bin; game.RS3Applet
+"C:\Program Files\Java\jre1.8.0_151\bin\java.exe" -Xms128m -Xmx800m -Xss2m -Dsun.java2d.noddraw=true -XX:CompileThreshold=10000 -cp bin; game.RS3Applet
 pause
